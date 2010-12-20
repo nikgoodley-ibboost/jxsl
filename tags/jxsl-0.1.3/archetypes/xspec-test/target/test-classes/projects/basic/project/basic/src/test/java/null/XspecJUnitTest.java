0981321ce3409bb1f1da087d1b918a0a64a30518
benoit.mercibe@gmail.com <benoit.mercibe@gmail.com@25db27bc-df01-11de-95c9-073ad895b44c>
2010-12-20 19:13:11 +0000

    [maven-release-plugin]  copy for tag jxsl-0.1.3

---
+++ tags/jxsl-0.1.3/archetypes/xspec-test/target/test-classes/projects/basic/project/basic/src/test/java/null/XspecJUnitTest.java
@@ -0,0 +1,15 @@
+
+package ${package};
+
+import java.io.File;
+import com.servicelibre.jxsl.scenario.test.xspec.XspecScenarioJUnitTest;
+
+public class XspecJUnitTest extends XspecScenarioJUnitTest
+{
+
+    public XspecJUnitTest(File xspecFile)
+    {
+        super(xspecFile);
+    }
+
+}
