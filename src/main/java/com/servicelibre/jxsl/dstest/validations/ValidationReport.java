ea452194535de8acdd2eacd7865ce4a0dd3b9ea2
benoit.mercibe <benoit.mercibe@gmail.com>
2011-02-09 16:45:31 +0000

    add license information

--- src/main/java/com/servicelibre/jxsl/dstest/validations/ValidationReport.java
+++ src/main/java/com/servicelibre/jxsl/dstest/validations/ValidationReport.java
@@ -1,3 +1,23 @@
+/**
+ * Java XSL code library
+ *
+ * Copyright (C) 2010 Benoit Mercier <info@servicelibre.com> — All rights reserved.
+ *
+ * This file is part of jxsl.
+ *
+ * jxsl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * jxsl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with jxsl.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.servicelibre.jxsl.dstest.validations;
 
 import java.util.ArrayList;
