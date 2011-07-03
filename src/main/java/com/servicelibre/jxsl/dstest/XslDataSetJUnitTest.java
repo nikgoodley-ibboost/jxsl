87bb3d5bef0afd76a965135d18542f5cb2bb9057
benoit.mercibe <benoit.mercibe@gmail.com>
2011-07-03 00:53:41 +0000

    cosmetic changes

--- src/main/java/com/servicelibre/jxsl/dstest/XslDataSetJUnitTest.java
+++ src/main/java/com/servicelibre/jxsl/dstest/XslDataSetJUnitTest.java
@@ -38,66 +38,66 @@ import com.servicelibre.jxsl.dstest.validations.ValidationReport;
 
 @RunWith(Parameterized.class)
 public class XslDataSetJUnitTest {
-    private static final String XSL_DATASET_RUNNER_BEAN_ID = "xslDataSetRunner";
+	private static final String XSL_DATASET_RUNNER_BEAN_ID = "xslDataSetRunner";
 
-    public final static String SPRING_CONTEXT_FILENAME = "xsldataset-context.xml";
+	public final static String SPRING_CONTEXT_FILENAME = "xsldataset-context.xml";
 
-    private static XslDataSetRunner runner;
+	private static XslDataSetRunner runner;
 
-    static {
-	ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(SPRING_CONTEXT_FILENAME);
-	runner = (XslDataSetRunner) ctx.getBean(XSL_DATASET_RUNNER_BEAN_ID);
-    }
-
-    protected DocumentId documentId;
-
-    public XslDataSetJUnitTest(DocumentId documentId) {
-	super();
-	this.documentId = documentId;
-    }
-
-    @Parameters
-    public static Collection<Object[]> getDocuments() {
-
-	List<Object[]> documents = new ArrayList<Object[]>(4);
-
-	for (DocumentId docId : runner.getDocSource().getDocumentIds()) {
-	    documents.add(new Object[] { docId });
+	static {
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(SPRING_CONTEXT_FILENAME);
+		runner = (XslDataSetRunner) ctx.getBean(XSL_DATASET_RUNNER_BEAN_ID);
 	}
 
-	return documents;
-    }
+	protected DocumentId documentId;
 
-    @Test
-    public void dataSetValidationTest() {
+	public XslDataSetJUnitTest(DocumentId documentId) {
+		super();
+		this.documentId = documentId;
+	}
 
-	assertEngineRun(runner.run(documentId));
+	@Parameters
+	public static Collection<Object[]> getDocuments() {
 
-    }
-    
-    public static void assertEngineRun(List<ValidationReport> validationReports) {
+		List<Object[]> documents = new ArrayList<Object[]>(4);
 
-	boolean errors = false;
-
-	assertNotNull(validationReports);
-
-	for (ValidationReport report : validationReports) {
-	    DocumentId documentId = report.getDocumentId();
-	    assertNotNull(documentId);
-	    List<ValidationFailure> failures = report.getFailures();
-	    assertNotNull(failures);
-	    if (failures.size() > 0) {
-		System.out.println("Some output validators failed for " + documentId);
-
-		for (ValidationFailure failure : failures) {
-		    System.out.println(documentId + " => " + failure.getValidatorName() + ": " + failure.getMessage());
-		    errors = true;
+		for (DocumentId docId : runner.getDocSource().getDocumentIds()) {
+			documents.add(new Object[] { docId });
 		}
 
-	    }
+		return documents;
 	}
-	assertTrue(errors);
 
-    }
+	@Test
+	public void dataSetValidationTest() {
+
+		assertEngineRun(runner.run(documentId));
+
+	}
+
+	public static void assertEngineRun(List<ValidationReport> validationReports) {
+
+		boolean errors = false;
+
+		assertNotNull(validationReports);
+
+		for (ValidationReport report : validationReports) {
+			DocumentId documentId = report.getDocumentId();
+			assertNotNull(documentId);
+			List<ValidationFailure> failures = report.getFailures();
+			assertNotNull(failures);
+			if (failures.size() > 0) {
+				System.out.println("Some output validators failed for " + documentId);
+
+				for (ValidationFailure failure : failures) {
+					System.out.println(documentId + " => " + failure.getValidatorName() + ": " + failure.getMessage());
+					errors = true;
+				}
+
+			}
+		}
+		assertTrue(errors);
+
+	}
 
 }
